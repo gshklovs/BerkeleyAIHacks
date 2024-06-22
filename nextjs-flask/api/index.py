b8b5ff26f0d5b7ea60c06124ffdf27d9b7bdd012
Yasper De Jong <yasperdejong@gmail.com>
2024-06-22 16:28:26 -0700

    DB connection

--- nextjs-flask/api/index.py
+++ nextjs-flask/api/index.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from dotenv import dotenv_values
+import singlestoredb as s2
 app = Flask(__name__)
+
+config = dotenv_values(".env")
+conn = s2.connect(host=config["SINGLESTORE_HOST"], port=config["SINGLESTORE_PORT"], user=config["SINGLESTORE_USER"], password=config["SINGLESTORE_PASSWD"], database="db_derek_e7ae6")
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"<p>{conn.show.databases()}</p>"
