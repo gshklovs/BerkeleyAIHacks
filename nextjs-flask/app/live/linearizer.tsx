ed036783e168271101f8ce38fff15d5543d45dbb
DCha2001 <derekcha213@gmail.com>
2024-06-23 02:59:13 -0700

    asdf

--- nextjs-flask/app/live/linearizer.tsx
+++ nextjs-flask/app/live/linearizer.tsx
@@ -4,23 +4,47 @@ import axios from 'axios';
 import { cn } from "@/lib/utils";
 import { AnimatedList } from "@/components/magicui/animated-list";
  
-interface Item {
-  name: string;
-  description: string;
-  icon: string;
-  color: string;
-  time: string;
-}
+
+function Notification(something : string) {
+    return (
+      <figure
+        className={cn(
+          "relative mx-auto min-h-fit w-full max-w-[400px] transform cursor-pointer overflow-hidden rounded-2xl p-4",
+          // animation styles
+          "transition-all duration-200 ease-in-out hover:scale-[103%]",
+          // light styles
+          "bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)]",
+          // dark styles
+          "transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+        )}
+      >
+        <div className="flex flex-row items-center gap-3">
+          <div className="flex flex-col overflow-hidden">
+            <p className="text-sm font-normal dark:text-white/60">
+              {something}
+            </p>
+          </div>
+        </div>
+      </figure>
+    );
+  };
+
+  
 
 export default function LinearizeText(){
-    const [data, setData] = useState({});
+    const [data, setData] = useState([])
+    
 
     function createList(){
         const ideas = Object.entries(data);
         const ideasLength = Object.entries(data).length
-        return(<AnimatedList>
-            ideas
-        </AnimatedList>)
+        let notifications = ideas;
+        return(    
+        <div className="relative flex max-h-[400px] min-h-[400px] w-full max-w-[32rem] flex-col overflow-hidden rounded-lg border bg-background p-6 shadow-lg">
+        <AnimatedList>
+            {ideas}
+        </AnimatedList>
+      </div>)
 
 
     }
@@ -46,5 +70,5 @@ export default function LinearizeText(){
         return () => clearInterval(interval);
       }, []);
 
-    return(<div></div>)
+    return(<div>{createList()}</div>)
 }
--- nextjs-flask/app/page.tsx
+++ nextjs-flask/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <LinearizeText/>
-      <RecordButton />
     </div>
   );
 }
