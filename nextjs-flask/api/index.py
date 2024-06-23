d3739f7a8b30f0dfa5275926881470a27d319a07
salaars <salaars@uci.edu>
2024-06-22 21:19:47 -0700

    fixed flask syntax

--- nextjs-flask/api/index.py
+++ nextjs-flask/api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from models.assisted_merge import assistive_merge
 from models.extract_nodes import extract_entities_and_relationships
 
@@ -11,13 +11,25 @@ def hello_world():
 
 
 @app.route("/api/merge", methods=["POST"])  # needs work
-def merge(paragraphs):
+def merge():
+    data = request.get_json()
+    paragraphs = data["paragraphs"]
     merged_text = assistive_merge(paragraphs)
     return merged_text
 
 
 @app.route("/api/extract", methods=["POST"])  # needs work
-def extract(merged_text, existing_entities=None, existing_relationships=None):
+def extract():
+    data = request.get_json()
+    merged_text = data["merged_text"]
+    if "existing_entities" not in data:
+        existing_entities = None
+    else:
+        existing_entities = data["existing_entities"]
+    if "existing_relationships" not in data:
+        existing_relationships = None
+    else:
+        existing_relationships = data["existing_relationships"]
     triplets = extract_entities_and_relationships(
         merged_text, existing_entities, existing_relationships
     )
